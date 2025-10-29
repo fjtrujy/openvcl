376dd66647a727101a96cb08de5de2299343d08e
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2025-10-29 16:05:16 +0100

    Fix some DMA tag sizes

--- examples/pdk/src/dma.h
+++ examples/pdk/src/dma.h
@@ -38,9 +38,9 @@ void pkGifStat();
 
 #define DMA_CALL_TAG(Addr, iQuadCount)	((((u64)(Addr)) << 32) | (0x5 << 28) | iQuadCount)
 #define DMA_RET_TAG(iQuadCount)					((((u64)(0x6)) << 28) | iQuadCount)
-#define DMA_REF_TAG(ADDR, COUNT) ((((unsigned long)ADDR) << 32) | (0x3 << 28) | COUNT )
-#define DMA_CNT_TAG(COUNT)       (((unsigned long)(0x1) << 28) | COUNT)
-#define DMA_END_TAG(COUNT)       (((unsigned long)(0x7) << 28) | COUNT)
+#define DMA_REF_TAG(ADDR, COUNT) ((((u64) ADDR) << 32) | (0x3 << 28) | COUNT )
+#define DMA_CNT_TAG(COUNT)       (((u64)  (0x1) << 28) | COUNT)
+#define DMA_END_TAG(COUNT)       (((u64)  (0x7) << 28) | COUNT)
 #define DMA_CHCR(DIR,MOD,ASP,TTE,TIE,STR) ( (((u32)DIR)<<0) | (((u32)MOD)<<2) | \
                                             (((u32)ASP)<<4) | (((u32)TTE)<<6) | \
                                             (((u32)TIE)<<7) | (((u32)STR)<<8) )
